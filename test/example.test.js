bca995708b7d67b6e2df2e6f42ee59716be6f52e
Kevin Chen <kcchen33@gmail.com>
2021-11-16 23:40:20 +0100

    added mocha tests

---
+++ test/example.test.js
@@ -0,0 +1,35 @@
+/*
+const assert = require('assert');
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+//CHANGE package.json test: mocha
+
+//set up global variable car
+let car;
+
+//before each IT test setup
+beforeEach(() => {
+    car = new Car();
+})
+
+//group of its
+describe('Car', () => {
+    //individual tests
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+});
+*/

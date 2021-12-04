17c615851efd20759b858638586030d881130c7d
Kevin Chen <kcchen33@gmail.com>
2021-12-04 14:39:52 +0100

    implement first form solution

--- ceres-react-frontend/src/components/forms/CreateGame.js
+++ ceres-react-frontend/src/components/forms/CreateGame.js
@@ -1,41 +1,197 @@
-import * as React from 'react';
-import Navbar from '../navbar/Navbar';
-import './Form.css';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
+import Navbar from '../navbar/Navbar';
+
+import {
+    Grid
+    , MenuItem
+    , Input
+    , Typography
+    , RadioGroup
+    , Radio
+    , FormHelperText
+    , InputLabel
+    , Select
+    , FormControl
+    , FormControlLabel
+    , Checkbox
+    , TextField
+    , FormGroup
+    , FormLabel
+    , Paper
+    , Button
+    , Box
+} from '@mui/material'
+import { width } from '@mui/system';
+
+//https://www.bezkoder.com/react-hook-form-material-ui-validation/
 function CreateGame() {
 
-    const [gameType, setGameType] = React.useState('');
+    const validationSchema = Yup.object().shape({
+        fullname: Yup.string().required('Fullname is required'),
+        username: Yup.string()
+            .required('Username is required')
+            .min(6, 'Username must be at least 6 characters')
+            .max(20, 'Username must not exceed 20 characters'),
+        email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+        password: Yup.string()
+            .required('Password is required')
+            .min(6, 'Password must be at least 6 characters')
+            .max(40, 'Password must not exceed 40 characters'),
+        confirmPassword: Yup.string()
+            .required('Confirm Password is required')
+            .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+        acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
+    });
 
-    const handleChange = (event) => {
-        setGameType(event.target.value);
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const onSubmit = data => {
+        console.log(JSON.stringify(data, null, 2));
     };
 
     return (
-        <div >
+        <div>
             <Navbar />
-            <div className="form-type">
-                <FormControl className="form-wrapper">
-                    <InputLabel htmlFor="my-input">Game Type</InputLabel>
-                    <Select
-                        labelId="select-game-type"
-                        id="select-game-type"
-                        value={gameType}
-                        label="gameType"
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={'weather'}>Weather</MenuItem>
-                        <MenuItem value={'flight'}>Flight</MenuItem>
-                    </Select>
-                    <FormHelperText id="my-helper-text">Select game type.</FormHelperText>
-                </FormControl>
-            </div>
-        </div >
+            <Paper sx={{margin: '2%'}}>
+                <Box px={3} py={2}>
+                    <Typography variant="h6" align="center" margin="dense">
+                        Create Game
+                    </Typography>
+
+                    <Grid container spacing={1}>
+                        <Grid item xs={12} sm={12}>
+                            <TextField
+                                required
+                                id="fullname"
+                                name="fullname"
+                                label="Full Name"
+                                fullWidth
+                                margin="dense"
+                                {...register('fullname')}
+                                error={errors.fullname ? true : false}
+                            />
+                            <Typography variant="inherit" color="textSecondary">
+                                {errors.fullname?.message}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="username"
+                                name="username"
+                                label="Username"
+                                fullWidth
+                                margin="dense"
+                                {...register('username')}
+                                error={errors.username ? true : false}
+                            />
+                            <Typography variant="inherit" color="textSecondary">
+                                {errors.username?.message}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="email"
+                                name="email"
+                                label="Email"
+                                fullWidth
+                                margin="dense"
+                                {...register('email')}
+                                error={errors.email ? true : false}
+                            />
+                            <Typography variant="inherit" color="textSecondary">
+                                {errors.email?.message}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="password"
+                                name="password"
+                                label="Password"
+                                type="password"
+                                fullWidth
+                                margin="dense"
+                                {...register('password')}
+                                error={errors.password ? true : false}
+                            />
+                            <Typography variant="inherit" color="textSecondary">
+                                {errors.password?.message}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="password"
+                                fullWidth
+                                margin="dense"
+                                {...register('confirmPassword')}
+                                error={errors.confirmPassword ? true : false}
+                            />
+                            <Typography variant="inherit" color="textSecondary">
+                                {errors.confirmPassword?.message}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={
+                                    <Controller
+                                        control={control}
+                                        name="acceptTerms"
+                                        defaultValue="false"
+                                        inputRef={register()}
+                                        render={({ field: { onChange } }) => (
+                                            <Checkbox
+                                                color="primary"
+                                                onChange={e => onChange(e.target.checked)}
+                                            />
+                                        )}
+                                    />
+                                }
+                                label={
+                                    <Typography color={errors.acceptTerms ? 'error' : 'inherit'}>
+                                        I have read and agree to the Terms *
+                                    </Typography>
+                                }
+                            />
+                            <br />
+                            <Typography variant="inherit" color="textSecondary">
+                                {errors.acceptTerms
+                                    ? '(' + errors.acceptTerms.message + ')'
+                                    : ''}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Box mt={3}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleSubmit(onSubmit)}
+                        >
+                            CREATE
+                        </Button>
+                    </Box>
+                </Box>
+            </Paper>
+        </div>
     )
 }
 

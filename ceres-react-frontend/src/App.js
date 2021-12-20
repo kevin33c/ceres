d79b6134f032a0e1d5e6d7c1a52d6c0334669b2c
Kevin Chen <kcchen33@gmail.com>
2021-12-20 19:46:38 +0100

    added mui reach alert component

--- ceres-react-frontend/src/App.js
+++ ceres-react-frontend/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/navbar/Navbar';
+import Alert from './components/utils/Alert';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
+      <Alert />
       <Navbar />
     </div>
   );

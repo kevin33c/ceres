26649af11865bed94a54f4241ca0e4cd9a1cf58c
Kevin Chen <kcchen33@gmail.com>
2021-12-24 23:56:25 +0100

    refactored games list

--- ceres-react-frontend/src/components/games/GamesList.js
+++ ceres-react-frontend/src/components/games/GamesList.js
@@ -12,13 +12,14 @@ import {
   , Card
   , CardMedia
   , Grid
+  , Chip
 } from '@mui/material';
 
 const games = new GamesServices();
 
 function GamesList() {
 
-  const [gamesList, setGamesList] = useState(['']);
+  const [gamesList, setGamesList] = useState([]);
 
   //trigger before the page load
   useEffect(() => {
@@ -40,8 +41,8 @@ function GamesList() {
       {gamesList &&
         <Container fixed>
           <Grid container spacing={2} sx={{ mt: '3%' }}>
-            {gamesList.map((game) => (
-              <Grid item xs={4} key={game.id}>
+            {gamesList?.map((game) => (
+              <Grid item xs={3} key={game?.id}>
                 <Card sx={{ maxWidth: 345 }}>
                   <CardMedia
                     component="img"
@@ -51,14 +52,14 @@ function GamesList() {
                   />
                   <CardContent>
                     <Typography noWrap gutterBottom variant="body2" component="div">
-                      {game.contract_address}
+                      {game?.contract_address}
                     </Typography>
-                    <Typography noWrap variant="body2" color="text.secondary">
-                      {game.name}
+                    <Typography noWrap variant="body2" color="text.secondary" component="div">
+                      <Chip label={game?.type} color="primary" size="small" /> <Chip label={game?.status} color="secondary" size="small" />
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Link to={`/game/${game.id}`}>
+                    <Link to={`/game/${game?.id}`}>
                       <Button size="small">
                         See Details
                       </Button>
